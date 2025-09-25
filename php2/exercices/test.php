a775c2c807bbf6483de67cc2d8d1d7314ea44232
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-09-25 15:30:14 +0200

    test fini

---
+++ php2/exercices/test.php
@@ -0,0 +1,10 @@
+<?php
+const MAX_SPEED = 130;
+$speed = 140;
+
+$speed = 110;
+
+echo "La vitesse maximum est de" . MAX_SPEED . "km/h et
+vous rouliez à $speed km/h";
+
+Phrase généré La vitesse maximum est de130km/h et vous rouliez à 110 km/h
