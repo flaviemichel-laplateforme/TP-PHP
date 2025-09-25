dbe8d82b18f1911ca0c57745d59b2d803e87214e
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-09-25 15:19:00 +0200

    exercice 1 fini

---
+++ php2/exercice1/me.php
@@ -0,0 +1,13 @@
+<?php
+
+// Crée un fichier me.php.
+// 2. Dans ce fichier, déclare trois variable contenant ton
+// prénom, ta ville et ton âge.
+// 3. Affiche la phrase :
+// Je m'appelle [x], j'ai [x] ans et j'habite à [x].
+
+$prenom = "flavie";
+$ville = "La Farlède";
+$age = 33;
+
+echo "Je m'appelle $prenom,j'ai $age.ans et j'habite à $ville.";
